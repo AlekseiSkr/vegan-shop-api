fdbbf88cef2088e940693fd0a79410a8920125e8
Aleksei Skorjak <43403562+AlekseiSkr@users.noreply.github.com>
2023-04-27 11:27:42 +0200

    Add project files.

---
+++ vegan-shop-api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vegan_shop_api.Models;
+
+namespace vegan_shop_api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<User> Users { get; set; }
+}
---
+++ vegan-shop-api/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using vegan_shop_api.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public DateTime OrderDate { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; }
+}
+
+public class OrderItem
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public int OrderId { get; set; }
+    public Order Order { get; set; }
+}
